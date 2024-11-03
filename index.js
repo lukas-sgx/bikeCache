8d55bbb9d1cd0dd08b38413f0fbb8658fa14a955
lukas soigneux <lukas@kali.picsou.com>
2024-11-03 18:54:26 +0100

    change env

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const io = new Server(server);
 
 const port = 8000;
 
-app.use(express.static(__dirname + '/static/'));
+app.use(express.static(__dirname + '/public/'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/login.html');
