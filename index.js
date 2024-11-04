0fb8789b315512e0e035ac7af174be171d0f5264
lukas soigneux <lukas@kali.picsou.com>
2024-11-04 21:34:15 +0100

    change env

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
+var bodyParser = require('body-parser');
 const { Server } = require("socket.io");
 const io = new Server(server);
 
 const port = 8000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
 
 app.use(express.static(__dirname + '/public/'));
 
@@ -13,7 +17,11 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/login.html');
 });
 
-
+app.post('/', (req, res) => {
+    console.log('receiving data ...');
+    console.log('body is ',req.body);
+    res.redirect('/')
+})
 
 io.on('connection', (socket) => {
     console.log('a user connected');
