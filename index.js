440d6bdbc442d26b20adb2cfe598ce6759cc5eb9
lukas soigneux <lukas@kali.picsou.com>
2024-11-05 23:16:08 +0100

    change env

--- index.js
+++ index.js
@@ -1,30 +1,52 @@
 const express = require('express');
+const cookieParser = require("cookie-parser");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
 var bodyParser = require('body-parser');
 const { Server } = require("socket.io");
 const io = new Server(server);
+const crypto = require("crypto");
 
 const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+app.use(cookieParser());
 
 app.use(express.static(__dirname + '/public/'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/login.html');
+    if(req.cookies.sessionId){
+        res.sendFile(__dirname + '/index.html');
+    }else{
+        res.sendFile(__dirname + '/login.html');
+    }
 });
 
 app.post('/', (req, res) => {
-    console.log('receiving data ...');
-    console.log('body is ',req.body);
-    res.redirect('/')
+    res.cookie('sessionId', crypto.randomBytes(12).toString("hex"), {
+        maxAge: 0.5 * 60 * 60 * 1000, // 30 minutes
+        httpOnly: false
+    });
+    res.cookie('sessionName', req.body.username, {
+        maxAge: 0.5 * 60 * 60 * 1000, // 30 minutes
+        httpOnly: false
+    });
+    res.cookie('sessionAction', req.body.action, {
+        maxAge: 0.5 * 60 * 60 * 1000, // 30 minutes
+        httpOnly: false
+    });
+
+    res.redirect('/');
 })
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+      });
 });
 
 server.listen(port, () => {
